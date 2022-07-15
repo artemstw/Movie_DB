f2f902df4ad53234b4d24bcca376be9f11dff9b5
Artem Stawetski <artemstw@gmail.com>
2022-07-15 20:46:27 +0300

    Выполняет практическое задание по работе с сайтом

--- js/script.js
+++ js/script.js
@@ -1,50 +1,87 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту
-
-5) Добавить нумерацию выведенных фильмов */
-
 "use strict";
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против...",
-    ],
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Мстители: Война бесконечности",
+            "Марсианин",
+            "Покемон: детектив Пикачу",
+            "Властелин колец",
+        ],
+    };
 
-const adv = document.querySelectorAll(".promo__adv img"),
-    poster = document.querySelector(".promo__bg"),
-    movieGenre = poster.querySelector(".promo__genre"),
-    movieList = document.querySelector(".promo__interactive-list");
+    const adv = document.querySelectorAll(".promo__adv img"),
+        poster = document.querySelector(".promo__bg"),
+        movieGenre = poster.querySelector(".promo__genre"),
+        movieList = document.querySelector(".promo__interactive-list"),
+        addForm = document.querySelector("form.add"), // Ищем форму, у которой есть класс .add
+        addInput = addForm.querySelector(".adding__input"), //Находим input внутри формы addForm
+        checkbox = addForm.querySelector('[type = "checkbox"]'); //Находим checkbox прямо через атрибут
 
-adv.forEach((item) => {
-    item.remove();
-});
+    addForm.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-movieGenre.textContent = "драма";
+        let newFilm = addInput.value;
+        const favourite = checkbox.checked;
 
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
 
-movieList.innerHTML = "";
+            if (favourite) {
+                console.log("Добавляем любимый фильм");
+            }
 
-movieDB.movies.sort();
+            movieDB.movies.push(newFilm); //Добавляет в базу данных - массив movies еще один фильм в строковом формате
+            sortArray(movieDB.movies); //Сортирует все фильмы в movies по алфавиту
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i + 1} ${film}
-        <div class="delete"></div>
-    </li>
-    `;
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach((item) => {
+            item.remove();
+        });
+    };
+
+    const makeChanges = () => {
+        movieGenre.textContent = "драма";
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    function sortArray(arr) {
+        arr.sort();
+    }
+
+    function createMovieList(films, parent) {
+        //Функция динамически добавляет новый фильм в список с номером
+        parent.innerHTML = ""; //Заменяет содержимое пустой строкой
+        sortArray(films);
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+        `;
+        });
+
+        document.querySelectorAll(".delete").forEach((btn, i) => {
+            //Удаляет фильм из списка при клике на корзину
+            btn.addEventListener("click", () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList(films, parent); //Заново строим список с помощью рекурсии
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
 });
